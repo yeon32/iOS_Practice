16321cd6f25659a8ddd15bdf1cea5cb1ee69ca32
yeon32 <32jyeon@gamil.com>
2022-07-30 22:05:31 +0900

    Initial Commit

---
+++ LifeCycle-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle-1
+//
+//  Created by 김정연 on 2022/07/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
