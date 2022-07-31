fd5387397401be2a8dfc321d51c8f290c6ab0633
yeon32 <32jyeon@gamil.com>
2022-07-31 17:40:23 +0900

    LifeCycle 실습

--- LifeCycle-1/ViewController.swift
+++ LifeCycle-1/ViewController.swift
@@ -9,9 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var uITextField: UITextField!
+    
+    @IBAction func didTapAction(_ sender: Any) {
+        
+        let data : String = uITextField.text!
+        
+        //데이터 넘겨주기
+        let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        
+        secondViewController.result = data
+        
+        self.present(secondViewController, animated: true, completion: nil)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("First: viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("First: viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("First: viewDidAppear")
+
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("First: viewDidDisappear")
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("First: viewWillDisappear")
+
     }
 
 
